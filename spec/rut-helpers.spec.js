592039d75b1024ca7e6755bf5d5e51ca2f349faa
Guillermo Moreno <gmq@guillermomoreno.cl>
2016-12-23 13:44:51 -0300

    tests(): add validator tests

--- spec/rut-helpers.spec.js
+++ spec/rut-helpers.spec.js
@@ -1,2 +1,23 @@
-import Jasmine from 'jasmine';
+/* eslint-disable no-magic-numbers */
 import * as rutHelpers from '../src/rut-helpers.js';
+
+
+describe('rutHelper', () => {
+  describe('rutHelper.rutValidate', () => {
+    it('should return true if string is a valid rut', () => {
+      expect(rutHelpers.rutValidate('7.618.285-K')).toBe(true);
+    });
+
+    it('should return false if string is not a valid rut', () => {
+      expect(rutHelpers.rutValidate('7.618.212-K')).toBe(false);
+    });
+
+    it('should return false if string is empty', () => {
+      expect(rutHelpers.rutValidate('')).toBe(false);
+    });
+
+    it('should return false if string is not a string', () => {
+      expect(rutHelpers.rutValidate(194453426)).toBe(false);
+    });
+  });
+});
