f51f2656277ea16b541c41aa4f6e4c03f12362df
Rafael Poveda <raerpo@gmail.com>
2017-09-21 18:10:10 -0300

    add return type to functions and use implicit typing for easier reading

--- src/rut-helpers.ts
+++ src/rut-helpers.ts
@@ -1,16 +1,16 @@
-export function rutClean(value: string) {
+export function rutClean(value: string): string {
   return typeof value === 'string' ? value.replace(/[^0-9kK]+/g, '').toUpperCase() : '';
 }
 
-export function rutValidate(value: string) {
+export function rutValidate(value: string): boolean {
   if (typeof value !== 'string') {
     return false;
   }
 
-  const rut: string = rutClean(value);
-  let rutDigits: number = parseInt(rut.slice(0, -1), 10);
-  let m: number = 0;
-  let s: number = 1;
+  const rut = rutClean(value);
+  let rutDigits = parseInt(rut.slice(0, -1), 10);
+  let m = 0;
+  let s = 1;
 
   while (rutDigits > 0) {
     s = (s + rutDigits % 10 * (9 - m++ % 6)) % 11;
@@ -21,15 +21,15 @@ export function rutValidate(value: string) {
   return (checkDigit === rut.slice(-1));
 }
 
-export function rutFormat(value: string) {
-  const rut: string = rutClean(value);
+export function rutFormat(value: string): string {
+  const rut = rutClean(value);
 
   if (rut.length <= 1) {
     return rut;
   }
 
-  let result: string = `${rut.slice(-4, -1)}-${rut.substr(rut.length - 1)}`;
-  for (let i: number = 4; i < rut.length; i += 3) {
+  let result = `${rut.slice(-4, -1)}-${rut.substr(rut.length - 1)}`;
+  for (let i = 4; i < rut.length; i += 3) {
     result = `${rut.slice(-3 - i, -i)}.${result}`;
   }
 
